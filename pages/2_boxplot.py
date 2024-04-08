7778b20bcf4e63e8219807943e0c11b4fa2f3cbe
svyas226 <stutivyas226@gmail.com>
2024-04-08 19:52:47 -0400

    Create 2_boxplot.py

---
+++ pages/2_boxplot.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import altair as alt
+
+st.write('CMPD Traffic Stops')
+
+@st.cache_data
+def load_data(csv):
+    df = pd.read_csv(csv)
+    return df
+
+stops = load_data('data\Officer_Traffic_Stops.csv')
+
+## Box plot
+box = alt.Chart(stops).mark_boxplot().encode(
+    x = alt.X('Was_a_Search_Conducted'),
+    y = alt.Y('Driver_Age')
+).properties(
+    width = 500,
+    title = 'Boxplot between Search Conducted vs Driver Age')
+
+st.altair_chart(box)
